9967c280fc70105ed68e79c08d1ab67c9da7eec4
Mustafa <73131585+mustafa89@users.noreply.github.com>
2020-11-07 23:36:04 +0100

    :lipstick: Add some styling to make it pretyy. WEEEEE!

--- src/SearchBox.js
+++ src/SearchBox.js
@@ -4,7 +4,7 @@ const SearchBox = ({searchChange}) => {
     return(
         <div className='pa2'>
         <input 
-            className='tc pa3 ma2 bg-light-green' 
+            className='tc pa3 ma2 bg-light-green grow shadow-5' 
             type='search' 
             placeholder='Search Robots'
             onChange={searchChange}>
